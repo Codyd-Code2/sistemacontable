3db1a8d25f63ce21614bfb0f1122bb70a5d11e50
juan <jmdiazg.codyd@gmail.com>
2022-03-16 17:52:41 -0500

    prue

---
+++ modlibreria/controllers/controllers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class Modlibreria(http.Controller):
+#     @http.route('/modlibreria/modlibreria', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/modlibreria/modlibreria/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('modlibreria.listing', {
+#             'root': '/modlibreria/modlibreria',
+#             'objects': http.request.env['modlibreria.modlibreria'].search([]),
+#         })
+
+#     @http.route('/modlibreria/modlibreria/objects/<model("modlibreria.modlibreria"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('modlibreria.object', {
+#             'object': obj
+#         })
