830759a95b553ec4790dfda9ba6d623b5afcbb54
juan <jmdiazg.codyd@gmail.com>
2022-03-17 10:54:35 -0500

    controller1

--- modlibreria/controllers/controllers.py
+++ modlibreria/controllers/controllers.py
@@ -5,7 +5,7 @@ from odoo import http
 class Modlibreria(http.Controller):
      @http.route('/modlibreria/modlibreria', auth='public')
      def index(self, **kw):
-         return "Hello"
+         return "Hello "
 
 #     @http.route('/modlibreria/modlibreria/objects', auth='public')
 #     def list(self, **kw):
